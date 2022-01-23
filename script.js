d7d270124f35e64f96448a8d25bafd35b3a016ad
MatthaeusL <mat@lispage.com>
2022-01-23 21:12:53 +0100

    server update

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let users = [];
 
-function addUser() {
-    users.push('John);
-        backend.setItem('users', JSON.stringify(users));
-    }
-    async function addUser() {
-        users.push('John);
-            await backend.setItem('users', JSON.stringify(users));
-        }
-        async function init() {
-            await downloadFromServer();
-            users = JSON.parse(backend.getItem('users')) || [];
-        }
+// function addUser() {
+//     users.push('John');
+//     backend.setItem('users', JSON.stringify(users));
+// }
+async function addUser(name) {
+    users.push(name);
+    await backend.setItem('users', JSON.stringify(users));
 
-        function deleteUser(name) {
-            backend.deleteItem('users');
-        }
+}
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+}
+
+function deleteUser(name) {
+    backend.deleteItem('users')
+    users = [];
+}
