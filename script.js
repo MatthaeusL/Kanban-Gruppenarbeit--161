c9b99835d7a215f8c14ecfad417359485fc61dcb
MatthaeusL <mat@lispage.com>
2022-01-26 14:15:19 +0100

    added array , direct acces to inner arrays , find and filter function

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-let users = [];
+let kanbanArray = [{
+    'tasks': [{
+        'taskid': 'taskid_0',
+        'title': 'title',
+        'cathegory': 'cathegory',
+        'description': 'description',
+        'duedate': 'duedate',
+        'urgency': 'urgency',
+        'assignedTo': 'assignedTo',
+        'status': 'status',
+
+    }],
+    'users': [{
+        'userid': 'userid_0',
+        'username': 'username',
+        'email': 'email',
+        'key': 'key',
+
+    }, {
+        'userid': 'userid_1',
+        'username': 'username',
+        'email': 'email',
+        'key': 'key',
+
+    }, {
+        'userid': 'userid_2',
+        'username': 'username',
+        'email': 'email',
+        'key': 'key',
+
+    }]
+}]
+
+
+let users = {
+    'userid_1': 'userid_12',
+    'username': 'username',
+    'email': 'email',
+    'key': 'key',
+
+};
+
+let usersInArray = kanbanArray[0]["users"];
+let tasksInArray = kanbanArray[0]["tasks"];
+let findUser = usersInArray.find((usersInArray) => usersInArray.userid = 'userid_1');
+let filterUser = usersInArray.filter((usersInArray) => usersInArray.username = 'username');
+
+
+
 
 // function addUser() {
 //     users.push('John');
@@ -10,8 +58,8 @@ async function addUser(name) {
 
 }
 async function init() {
-    await downloadFromServer();
-    users = JSON.parse(backend.getItem('users')) || [];
+    // await downloadFromServer();
+    // users = JSON.parse(backend.getItem('users')) || [];
 }
 
 function deleteUser(name) {
