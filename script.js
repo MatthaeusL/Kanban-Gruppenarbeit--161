7df47a1e9a9f8e1ef94d870d90ae1b9ecd15718d
LeonDittrich <leon_dittrich@web.de>
2022-01-27 11:08:10 +0100

    Users

--- script.js
+++ script.js
@@ -15,18 +15,21 @@ let kanbanArray = [{
         'username': 'username',
         'email': 'email',
         'key': 'key',
+        'img': 'face2.jpg',
 
     }, {
         'userid': 'userid_1',
         'username': 'username',
         'email': 'email',
         'key': 'key',
+        'img': 'face3.jpg',
 
     }, {
         'userid': 'userid_2',
         'username': 'username',
         'email': 'email',
         'key': 'key',
+        'img': 'face5.jpg',
 
     }]
 }]
@@ -70,3 +73,26 @@ function deleteUser(name) {
 /**
  * 
  */
+
+ function backlogUsers(i) {
+    let userContainer = document.getElementById('backlog_users');
+    userContainer.innerHTML = '';
+    for (let x = 0; x < usersInArray.length; x++) {
+        userContainer.innerHTML += `
+        <div class="imgContainer3">
+            <img class="imgAvatar2" src="./img/${usersInArray['img']}">
+            <div style="padding-left: 1.5rem;">
+                <span>${usersInArray['username']}</span>
+                <a href="mailto:${usersInArray['email']}">${usersInArray['email']}}</a>
+            </div>
+        </div>
+
+        <div class="department">
+            <span>${i}</span>
+        </div>
+
+        <div class="details">
+            <span>${i}</span>
+        </div>`;
+    }    
+}
