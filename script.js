080710175fc98ff8aca71485151600e369d18d5d
LeonDittrich <leon_dittrich@web.de>
2022-01-27 16:12:36 +0100

    User einfügen mit username Problem

--- script.js
+++ script.js
@@ -12,24 +12,53 @@ let kanbanArray = [{
     }],
     'users': [{
         'userid': 'userid_0',
-        'username': 'username',
-        'email': 'email',
+        'username': 'Klaus Meier',
+        'email': 'Klaus_Meier@web.de',
         'key': 'key',
-        'img': 'face2.jpg',
+        'img': 'face1.jpg',
+        'cathegory': 'Marketing',
+        'color': '--bgMarketing',
+        'detail': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora dolore culpa optio aut deleniti vitae quod, placeat eius maxime quibusdam? Laudantium quam vel aperiam placeat.',
 
     }, {
         'userid': 'userid_1',
-        'username': 'username',
-        'email': 'email',
+        'username': 'Inav Bolski',
+        'email': 'InavBolski@Yahoo.de',
         'key': 'key',
         'img': 'face3.jpg',
+        'cathegory': 'Sale',
+        'color': '--bgSale',
+        'detail': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora dolore culpa optio aut deleniti vitae quod, placeat eius maxime quibusdam? Laudantium quam vel aperiam placeat.',
 
     }, {
         'userid': 'userid_2',
-        'username': 'username',
-        'email': 'email',
+        'username': 'Laura Trautmann',
+        'email': 'Laura-Trautmann@t-online.de',
         'key': 'key',
         'img': 'face5.jpg',
+        'cathegory': 'Design',
+        'color': '--bgDesign',
+        'detail': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora dolore culpa optio aut deleniti vitae quod, placeat eius maxime quibusdam? Laudantium quam vel aperiam placeat.',
+
+    }, {
+        'userid': 'userid_3',
+        'username': 'Tom Müller',
+        'email': 'MüllerTom@GMX.de',
+        'key': 'key',
+        'img': 'face2.jpg',
+        'cathegory': 'Sale',
+        'color': '--bgSale',
+        'detail': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora dolore culpa optio aut deleniti vitae quod, placeat eius maxime quibusdam? Laudantium quam vel aperiam placeat.',
+
+    }, {
+        'userid': 'userid_4',
+        'username': 'Karin Schneider',
+        'email': 'Schneider@web.de',
+        'key': 'key',
+        'img': 'face4.jpg',
+        'cathegory': 'Service',
+        'color': '--bgService',
+        'detail': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora dolore culpa optio aut deleniti vitae quod, placeat eius maxime quibusdam? Laudantium quam vel aperiam placeat.',
 
     }]
 }]
@@ -40,6 +69,9 @@ let users = {
     'username': 'username',
     'email': 'email',
     'key': 'key',
+    'img': 'img',
+    'cathegory': 'cathegory',
+    'color': 'color',
 
 };
 
@@ -55,20 +87,20 @@ let filterUser = usersInArray.filter((usersInArray) => usersInArray.username = '
 //     users.push('John');
 //     backend.setItem('users', JSON.stringify(users));
 // }
-async function addUser(name) {
+/*async function addUser(name) {
     users.push(name);
     await backend.setItem('users', JSON.stringify(users));
 
-}
+}*/
 async function init() {
     // await downloadFromServer();
     // users = JSON.parse(backend.getItem('users')) || [];
     click_nav_board()
+    backlogUsers()
 }
 
-function deleteUser(name) {
-    backend.deleteItem('users')
-    users = [];
+/*function deleteUser() {
+    backend.deleteItem(kanbanArray);
 }
 
 /**
@@ -130,26 +162,33 @@ function click_help() {
 }
 
 
-
-function backlogUsers(i) {
+/**
+ *  ***************User Einfügen*****************
+ * 
+ */
+async function backlogUsers() {
     let userContainer = document.getElementById('backlog_users');
     userContainer.innerHTML = '';
-    for (let x = 0; x < usersInArray.length; x++) {
+    for (let i = 0; i < usersInArray.length; i++) {
+        
         userContainer.innerHTML += `
-        <div class="imgContainer3">
-            <img class="imgAvatar2" src="./img/${usersInArray['img']}">
-            <div style="padding-left: 1.5rem;">
-                <span>${usersInArray['username']}</span>
-                <a href="mailto:${usersInArray['email']}">${usersInArray['email']}}</a>
+        <div id="backlog_user${i}" class="infoContainer">
+            <div class="imgContainer3">
+                <img class="imgAvatar2" src="./img/${usersInArray[i]['img']}">
+                <div class="row">
+                    <span>${usersInArray[i]['username']}</span>
+                    <a href="mailto:${usersInArray[i]['email']}">${usersInArray[i]['email']}</a>
+                </div>
             </div>
-        </div>
 
-        <div class="department">
-            <span>${i}</span>
-        </div>
+            <div class="department">
+                <span>${usersInArray[i]['cathegory']}</span>
+            </div>
 
-        <div class="details">
-            <span>${i}</span>
+            <div class="details">
+                <span>${usersInArray[i]['detail']}</span>
+            </div>
         </div>`;
+        document.getElementsByClassName('infoContainer')[i].style.borderLeftColor = `var(${usersInArray[i]['color']})`;
     }
 }
