5065831485f8d6c6e6e3a28c0eeaeaf7e8b4dc7c
LeonDittrich <leon_dittrich@web.de>
2022-02-07 21:29:11 +0100

    Help Container fast fertig

--- script.js
+++ script.js
@@ -460,6 +460,37 @@ function addMembersImg(userIDArrayimg) {
 // }
 
 
+/* --------------------------------------------------------Help Script----------------------------------------------- */
+function showHelp() {
+    let section = document.getElementById('description' + i);
+    let chevron = document.getElementById('chevron' + i);
+    if (notOpen(section)) {
+        openHelp(chevron, section);
+    }
+    else {
+        closeHelp(chevron, section);
+    }
+}
+
+function openHelp() {
+    let chevron = document.getElementById('chevron');
+    setTimeout(() => {
+        chevron.style.transform = 'rotate(180deg)'
+    }, 10);
+    document.getElementById('content').classList.remove('d-none');
+}    
+
+function closeHelp() {
+    let chevron = document.getElementById('chevron');
+    setTimeout(() => {
+        chevron.style.transform = 'rotate(180deg)'
+    }, 10);
+    document.getElementById('content').classList.add('d-none');
+}
+
+function notOpen(section) {
+    return section.classList.contains('dNone');
+}
 /**
  * *******************************************************responsive script ******************************************
  */
