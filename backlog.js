b0e26e43e886e3e9abeafb0fb7e00576269ef702
Viktor Säubert <vsaeubert@gmail.com>
2022-02-10 15:50:06 +0100

    JSDoc bei Backlog.js

--- backlog.js
+++ backlog.js
@@ -1,5 +1,8 @@
 let backlogEmpty;
 
+/**
+ * Rendering Backlog Entries by task status 'backlog'
+ */
 async function backlogTasks() {
     let filterStatusBacklog = kanbanArray[0]["tasks"].filter((k) => k.status == 'backlog');
     backlogEmpty = filterStatusBacklog.length;
@@ -14,30 +17,13 @@ async function backlogTasks() {
     }
 }
 
-function generateUserImgs(i, filterStatusBacklog) {
-    let img = document.getElementById(`stackedImgBL${i}`);
-    let amountUser = filterStatusBacklog[i]['assignedTo'].length;
-    for (let j = 0; j < amountUser; j++) {
-        let assignedUserIndex = filterStatusBacklog[i]['assignedTo'][j];
-
-        img.innerHTML += `
-            <img class="imgAvatar2 stackedImgBL${j}" onclick="showBLusers(${i})" src="./img/${kanbanArray[0]["users"][assignedUserIndex]['img']}">
-            `;
-        document.getElementById(`BLhiddenUserAssigned${i}`).innerHTML += `
-            <img class="imgAvatar2 stackedImgBL${j}" onclick="showBLusers(${i})" src="./img/${kanbanArray[0]["users"][assignedUserIndex]['img']}">
-            `;
-    }
-}
-
-function showBLusers(id) {
-    document.getElementById(`BLhiddenUserAssigned${id}`).classList.remove('d-none');
-}
-
-function closeUsersBL(id) {
-    document.getElementById(`BLhiddenUserAssigned${id}`).classList.add('d-none');
-}
-
-
+/**
+ * Generating HTML Backlog entries
+ * @param {number} i -- Task ID
+ * @param {number} currentUserID  -- Current USer ID, first User
+ * @param {*} filterStatusBacklog  --     JSON with Backlog Tasks
+ * @returns 
+ */
 async function generateBacklogHTML(i, currentUserID, filterStatusBacklog) {
     return `
     <div id="backlog_user${i}" >
@@ -63,6 +49,46 @@ async function generateBacklogHTML(i, currentUserID, filterStatusBacklog) {
     </div>`;
 }
 
+/**
+ * Rendering one or more Images of User
+ * @param {*} i 
+ * @param {*} filterStatusBacklog 
+ */
+function generateUserImgs(i, filterStatusBacklog) {
+    let img = document.getElementById(`stackedImgBL${i}`);
+    let amountUser = filterStatusBacklog[i]['assignedTo'].length;
+    for (let j = 0; j < amountUser; j++) {
+        let assignedUserIndex = filterStatusBacklog[i]['assignedTo'][j];
+
+        img.innerHTML += `
+            <img class="imgAvatar2 stackedImgBL${j}" onclick="showBLusers(${i})" src="./img/${kanbanArray[0]["users"][assignedUserIndex]['img']}">
+            `;
+        document.getElementById(`BLhiddenUserAssigned${i}`).innerHTML += `
+            <img class="imgAvatar2 stackedImgBL${j}" onclick="showBLusers(${i})" src="./img/${kanbanArray[0]["users"][assignedUserIndex]['img']}">
+            `;
+    }
+}
+
+/**
+ * Shows all Users assigned to Task, open separate Div
+ * @param {number} id - Task ID
+ */
+function showBLusers(id) {
+    document.getElementById(`BLhiddenUserAssigned${id}`).classList.remove('d-none');
+}
+
+/**
+ * Hides above opened Div
+ * @param {number} id  Task ID
+ */
+function closeUsersBL(id) {
+    document.getElementById(`BLhiddenUserAssigned${id}`).classList.add('d-none');
+}
+
+/**
+ * Moves clicked Entry to Board/To do
+ * @param {number} m -- Task ID
+ */
 function shiftToBoard(m) {
     let arrayIndex = kanbanArray[0]["tasks"].findIndex((id) => id.taskid == m); // Filter to find the index of current Taskid
     kanbanArray[0]["tasks"][arrayIndex]['status'] = 'todo';
@@ -72,6 +98,11 @@ function shiftToBoard(m) {
     renderBoard()
 }
 
+/**
+ * Searches User ID by CurrentUser Parameter
+ * @param {text} currentUser 
+ * @returns Current User ID as number
+ */
 async function getUserID(currentUser) {
     for (let i = 0; i < kanbanArray[0]['users'].length; i++) {
         if (kanbanArray[0]['users'][i]['username'] == currentUser) {
@@ -80,6 +111,9 @@ async function getUserID(currentUser) {
     }
 }
 
+/**
+ * If there are no Tasks with Status'backlog', an extra Information is showed
+ */
 function checkBacklogEmpty() {
     backlogTasks();
     if (backlogEmpty == 0) {
